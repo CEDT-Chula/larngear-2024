afe64e3afd7832ad405ca7e55959493c101e6c46
Nitchakan <nitchakan.kkng@gmail.com>
2024-09-16 15:37:56 +0700

    add home page and leaderboard

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import NotFound from "./routes/NotFound";
 import DemoGame from "./routes/DemoGame";
+import Leaderboard from "./routes/LeaderBoard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/DemoGame" element={<DemoGame />} />
+        <Route path="/LeaderBoard" element={<Leaderboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- fe/src/routes/Home.jsx
+++ fe/src/routes/Home.jsx
@@ -1,10 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 const Home = () => {
   const navigate = useNavigate();
 
   const handleStartClick = () => {
     navigate("/DemoGame");
+  };
+
+  const [playerMode, setPlayerMode] = useState("one");
+
+  const handleModeChange = (event) => {
+    setPlayerMode(event.target.value);
   };
 
   return (
@@ -20,6 +27,42 @@ const Home = () => {
           onClick={handleStartClick}
         >
           Start
+        </button>
+      </div>
+
+      <div className="flex flex-col items-center gap-5 mt-[80px]">
+        <label>
+          <input
+            type="radio"
+            className="nes-radio"
+            name="playerMode"
+            value="one"
+            checked={playerMode === "one"}
+            onChange={handleModeChange}
+          />
+          <span>1 Player</span>
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            className="nes-radio"
+            name="playerMode"
+            value="two"
+            checked={playerMode === "two"}
+            onChange={handleModeChange}
+          />
+          <span>2 Player</span>
+        </label>
+      </div>
+
+      <div className="flex flex-col items-center gap-5 mt-[80px]">
+        <button
+          type="button"
+          className="px-10 py-2 nes-btn"
+          onClick={handleStartClick}
+        >
+          Exit
         </button>
         <img src="src/assets/subaru-duck-pixel.gif" alt="subaru duck dancing" />
       </div>
