c5104f178b5098e0d7fcd347c626f4081d717ec8
Wuaimi <10327sornpicha@gmail.com>
2024-10-15 14:23:16 +0700

    fixed baseEnemy and icecream enemy

--- fe/src/classes/enemies/BaseEnemy.ts
+++ fe/src/classes/enemies/BaseEnemy.ts
@@ -1,11 +1,17 @@
 import { GameController } from "../util/GameController";
 
 export class BaseEnemy extends Phaser.GameObjects.Sprite {
+    Name: string;
+    Name_Color: string;
     currentHealth: number;
     maxHealth: number;
     speed: number;
     baseSpeed: number;
     attack: number;
+    isPoisonResist: boolean;
+    isBurnResist: boolean;
+    isSlowResist: boolean;
+    isFreezeResist: boolean;
     sprite: string;
     path: Phaser.Curves.Path;
     pathPosition: number;
@@ -13,9 +19,15 @@ export class BaseEnemy extends Phaser.GameObjects.Sprite {
 
     constructor(
         scene: Phaser.Scene,
+        Name: string,
+        Name_Color: string,
         maxHealth: number,
         speed: number,
         attack: number,
+        isPoisonResist: boolean = true,
+        isBurnResist: boolean = true,
+        isSlowResist: boolean = true,
+        isFreezeResist: boolean = false,
         sprite: string,
         path: Phaser.Curves.Path
     ) {
@@ -23,11 +35,17 @@ export class BaseEnemy extends Phaser.GameObjects.Sprite {
 
         const gameController = GameController.getInstance();
 
+        this.Name = Name;
+        this.Name_Color = Name_Color;
         this.maxHealth = maxHealth * gameController.enemyHealth_Multiplier;
         this.currentHealth = this.maxHealth;
         this.baseSpeed = speed;
         this.speed = speed * gameController.enemySpeed_Multiplier;
         this.attack = attack;
+        this.isPoisonResist = isPoisonResist;
+        this.isBurnResist = isBurnResist;
+        this.isSlowResist = isSlowResist;
+        this.isFreezeResist = isFreezeResist;
         this.sprite = sprite;
         this.path = path;
         this.pathPosition = 0;
@@ -35,10 +53,10 @@ export class BaseEnemy extends Phaser.GameObjects.Sprite {
 
         this.setOrigin(0);
         this.setScale(4);
+        this.setTexture(sprite);
     }
 
     applySpeed(gameSpeed: number) {
-
         this.speed = this.baseSpeed * gameSpeed;
     }
 
@@ -54,24 +72,24 @@ export class BaseEnemy extends Phaser.GameObjects.Sprite {
             this.destroy(); // Destroy the enemy when it reaches the end
         }
     }
+
     takeDamage(dmg: number) {
         this.currentHealth -= dmg;
 
         if (this.currentHealth <= 0) {
-            this.onDeath()
-            this.destroy(true)
+            this.onDeath();
+            this.destroy(true);
         }
     }
 
     onArrived() {
-        // TODO: decrease Player's base hp by attack
-
+        // Decrease Player's base hp by attack
         console.log(this.sprite, " reached the end!");
 
         GameController.getInstance().playerHealth -= this.attack;
 
         if (GameController.getInstance().playerHealth <= 0) {
-            GameController.getInstance().gameOver('lose')
+            GameController.getInstance().gameOver('lose');
         }
 
         this.emit('onArrived');
